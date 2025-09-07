026bda9ad479185a6433be3a72154138e6c2cdc3
Brian Haw <brianshiroe@gmail.com>
2025-09-08 00:30:59 +0400

    [style & refactor] skills and assets
    - added rtl styles for icons in skills page
    - refactor assets.js to skills.ts
    - refactor naming convension from img to icons inside skills.ts file

--- app/[locale]/(public)/skills/page.tsx
+++ app/[locale]/(public)/skills/page.tsx
@@ -1,27 +1,39 @@
 // app/[locale]/(public)/skills/page.tsx
 "use client";
 
-import { skills } from '@/assets/assets.js'
-import Image from 'next/image';
+import { skills } from "@/assets/skills";
+import Image from "next/image";
 
 export default function SkillsPage() {
-
   return (
     <div className="">
       <h1 className="self-start text-4xl font-bold mb-10">Skills</h1>
 
       <section className="w-full max-w-5xl space-y-12">
         {skills.map((skills) => (
-          <div className="p-12 bg-gray-50 rounded-2xl inset-shadow-gray-400 inset-shadow-sm text-2xl font-semibold mb-6" 
-          key={skills.label}>
-            <h2 className="text-xl font-light text-gray-500 mb-10 pb-4 border-b-1 border-gray-300">{skills.label}</h2>
+          <div
+            className="p-12 bg-gray-50 rounded-2xl inset-shadow-gray-400 inset-shadow-sm text-2xl font-semibold mb-6"
+            key={skills.label}
+          >
+            <h2 className="text-xl font-light text-gray-500 mb-10 pb-4 border-b-1 border-gray-300">
+              {skills.label}
+            </h2>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
               {skills.tools.map((tools) => (
-                <div key={tools.name} className="flex items-center rounded-2xl shadow-lg p-4 hover:scale-103 
-                transition-transform bg-white w-2sm">
-                    {tools.img && (
-                    <Image src={tools.img} height={40} width={40} alt='' className='-translate-y-5 -translate-x-8 ' />
-                    )}
+                <div
+                  key={tools.name}
+                  className="flex items-center rounded-2xl shadow-lg p-4 hover:scale-103 
+                transition-transform bg-white w-2sm"
+                >
+                  {tools.icon && (
+                    <Image
+                      src={tools.icon}
+                      height={40}
+                      width={40}
+                      alt=""
+                      className="-translate-y-5 ltr:-translate-x-8 rtl:translate-x-8"
+                    />
+                  )}
                   <div className="text-lg font-medium text-gray-800">
                     {tools.name}
                   </div>
